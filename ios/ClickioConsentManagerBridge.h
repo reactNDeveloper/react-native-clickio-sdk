be38113e13ad271bcfe2d7790bf191f8c44437dc
Anna Sargsyan <anna.sargsyan@globaldev.tech>
2025-04-18 19:04:03 +0400

    changed file structure

--- ios/ClickioConsentManagerBridge.h
+++ ios/ClickioConsentManagerBridge.h
@@ -1,52 +1,24 @@
 // ClickioConsentManagerBridge.h
-//  clickioapp
-//
-//  Created by Admin on 02.04.25.
-//
-
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventDispatcher.h>
 #import <ClickioConsentSDKManager/ClickioConsentSDKManager-umbrella.h>
 
-
+// This file should NOT contain the @interface declaration for the module
 @interface RCT_EXTERN_MODULE(ClickioConsentManagerModule, NSObject)
 
-// Declare all methods here
-
+// Declare the methods you want to expose to React Native here
 RCT_EXTERN_METHOD(testConnection:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(initializeConsentSDK:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(openDialog:(RCTResponseSenderBlock)callback)
 RCT_EXTERN_METHOD(getConsentData:(RCTResponseSenderBlock)callback)
-RCT_EXTERN_METHOD(requestATTPermission:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(getGoogleConsentFlags:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(requestATTPermission:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getGoogleConsentFlags:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(sendManualConsentToFirebase)
 RCT_EXTERN_METHOD(checkAndSetConsentFlags)
-RCT_EXTERN_METHOD(isFirebaseAvailable:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(isAdjustAvailable:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(isAirbridgeAvailable:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(isAppsFlyerAvailable:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(isFirebaseAvailable:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(isAdjustAvailable:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(isAirbridgeAvailable:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(isAppsFlyerAvailable:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(setClickioLogging:(BOOL)isEnabled)
 
-
 @end
-
-
-// // Exposing the module to React Native
-// RCT_EXTERN_MODULE(ClickioConsentManagerModule, NSObject)
-
-// // Methods to be exposed to React Native
-// // RCT_EXTERN_METHOD(showConsentDialog)
-// // RCT_EXTERN_METHOD(initializeConsentSDK:callback:)
-// // RCT_EXTERN_METHOD(openDialog:callback:)
-
-// // This ensures the header is protected from multiple imports
-// #ifndef ClickioConsentManagerBridge_h
-// #define ClickioConsentManagerBridge_h
-// #endif
-
